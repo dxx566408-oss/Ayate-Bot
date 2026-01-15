1ddde837889efdbcbc23ea8268c87e31c818053b
dxx566408-oss <dxx566408@gmail.com>
2026-01-15 15:14:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -78,7 +78,7 @@ async def on_message(message):
 app = Flask('')
 @app.route('/')
 def home(): return "Bot is running"
-def run(): app.run(host='0.0.0.0', port=8080)
+def run(): app.run(host='0.0.0.0', port=10000)
 def keep_alive():
     t = Thread(target=run)
     t.start()
